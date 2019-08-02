d5b1eb9613127658af35a6ea02a81a4c855284dc
Marce Ramirez <marceram87@gmail.com>
2019-08-02 14:32:34 +0200

    Started Main section

---
+++ js/carousel.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const carousel = () => {
+  console.log("Carousel")
+}
--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 'use strict';
+
 const main = () => {
-  console.log("test")
+  console.log("Main");
+  carousel();
 }
 window.addEventListener('load', main);
